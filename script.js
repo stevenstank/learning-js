908574046c5d3487e4ea73ff19999329d482a3c8
stevenstank <stevenstank775@gmail.com>
2026-02-08 19:32:10 +0000

    update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let obj = {
   get propName() {
-    // getter, the code executed on getting obj.propName
+    
   },
 
   set propName(value) {
-    // setter, the code executed on setting obj.propName = value
+    
   }
 };
